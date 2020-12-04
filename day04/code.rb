bf510251f73ae6352b595b3ece3deac750ee0ae5
Rider Sargent <rider.sargent@gmail.com>
2020-12-04 12:40:26 +0000

    Day 4, part 2 and refactor day 4.

--- day04/code.rb
+++ day04/code.rb
@@ -1,31 +1,99 @@
 # frozen_string_literal: true
 
 def part_one(input)
-  passport = []
-  valid_passports = []
+  parse_passports(input).filter { |p| valie_fields? p }.length
+end
+
+def part_two(input)
+  parse_passports(input).filter { |p| valid_values? p }.length
+end
+
+private
+
+def parse_passports(input)
   input << ''
+  passport = []
+  passports = []
 
   input.each do |line|
     if line.empty?
-      valid_passports << passport if valid? passport
+      passports << passport.join(' ')
       passport = []
     else
       passport << line
     end
   end
 
-  valid_passports.length
+  passports
 end
 
-def valid?(passport)
-  entire_passport = passport.join
+def valie_fields?(passport)
+  [
+    passport.include?('byr:'),
+    passport.include?('iyr:'),
+    passport.include?('eyr:'),
+    passport.include?('hgt:'),
+    passport.include?('hcl:'),
+    passport.include?('ecl:'),
+    passport.include?('pid:')
+  ].all?
+end
 
-  entire_passport.include?('byr:') &&
-    entire_passport.include?('iyr:') &&
-    entire_passport.include?('eyr:') &&
-    entire_passport.include?('hgt:') &&
-    entire_passport.include?('hcl:') &&
-    entire_passport.include?('ecl:') &&
-    entire_passport.include?('pid:')
+def valid_values?(passport)
+  [
+    valid_birth_year?(passport),
+    valid_issue_year?(passport),
+    valid_expiration_year?(passport),
+    valid_height?(passport),
+    valid_hair_color?(passport),
+    valid_eye_color?(passport),
+    valid_passport_id?(passport)
+  ].all?
+end
+
+def valid_birth_year?(passport)
+  passport.match(/byr:(\d{4})/) do |v|
+    v[1].to_i >= 1920 && v[1].to_i <= 2002
+  end
+end
+
+def valid_issue_year?(passport)
+  passport.match(/iyr:(\S+)/) do |v|
+    v[1].to_i >= 2010 && v[1].to_i <= 2020
+  end
+end
+
+def valid_expiration_year?(passport)
+  passport.match(/eyr:(\S+)/) do |v|
+    v[1].to_i >= 2020 && v[1].to_i <= 2030
+  end
+end
+
+def valid_height?(passport)
+  !!passport.match(/hgt:(\S+)/) do |v|
+    h, units = v[1].match(/(\d+)(in|cm)/)&.values_at(1, 2)
+    case units
+    when 'cm'
+      h.to_i >= 150 && h.to_i <= 193
+    when 'in'
+      h.to_i >= 59 && h.to_i <= 76
+    end
+  end
+end
+
+def valid_hair_color?(passport)
+  !!passport.match(/hcl:#([0-9a-f]{6})/)
+end
+
+def valid_eye_color?(passport)
+  passport.match(/ecl:(\S{3})/) do |v|
+    %w[amb blu brn gry grn hzl oth].include? v[1]
+  end
+end
+
+def valid_passport_id?(passport)
+  passport.match(/pid:(\S+)/) do |v|
+    v[1].length == 9
+  end
 end
 
--- day04/tests.rb
+++ day04/tests.rb
@@ -32,6 +32,41 @@ def example_input
   ]
 end
 
+def invalid_passports
+  [
+    'eyr:1972 cid:100',
+    'hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926',
+    '',
+    'iyr:2019',
+    'hcl:#602927 eyr:1967 hgt:170cm',
+    'ecl:grn pid:012533040 byr:1946',
+    '',
+    'hcl:dab227 iyr:2012',
+    'ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277',
+    '',
+    'hgt:59cm ecl:zzz',
+    'eyr:2038 hcl:74454a iyr:2023',
+    'pid:3556412378 byr:2007'
+  ]
+end
+
+def valid_passports
+  [
+    'pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980',
+    'hcl:#623a2f',
+    '',
+    'eyr:2029 ecl:blu cid:129 byr:1989',
+    'iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm',
+    '',
+    'hcl:#888785',
+    'hgt:164cm byr:2001 iyr:2015 cid:88',
+    'pid:545766238 ecl:hzl',
+    'eyr:2022',
+    '',
+    'iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719'
+  ]
+end
+
 class TestDay < Minitest::Test
   def test_part_one_example
     assert_equal(2, part_one(example_input))
@@ -39,6 +74,61 @@ class TestDay < Minitest::Test
 
   def test_part_one
     assert_equal(239, part_one(input))
+  end
+
+  def test_part_two_with_valid_passports
+    assert_equal(4, part_two(valid_passports))
+  end
+
+  def test_part_two_with_invalid_passports
+    assert_equal(0, part_two(invalid_passports))
+  end
+
+  def test_valid_birth_year
+    assert_equal(true, valid_birth_year?('byr:2002'))
+  end
+
+  def test_invalid_birth_year
+    assert_equal(false, valid_birth_year?('byr:2003'))
+  end
+
+  def test_valid_height
+    assert_equal(true, valid_height?('hgt:60in'))
+    assert_equal(true, valid_height?('hgt:190cm'))
+  end
+
+  def test_invalid_height
+    assert_equal(false, valid_height?('hgt:190'))
+    assert_equal(false, valid_height?('hgt:190in'))
+  end
+
+  def test_valid_hair_color
+    assert_equal(true, valid_hair_color?('hcl:#123abc'))
+  end
+
+  def test_invalid_hair_color
+    assert_equal(false, valid_hair_color?('hcl:#123abz'))
+    assert_equal(false, valid_hair_color?('hcl:123abc'))
+  end
+
+  def test_valid_eye_color
+    assert_equal(true, valid_eye_color?('ecl:brn'))
+  end
+
+  def test_invalid_eye_color
+    assert_equal(false, valid_eye_color?('ecl:wat'))
+  end
+
+  def test_valid_passport_id
+    assert_equal(true, valid_passport_id?('pid:000000001'))
+  end
+
+  def test_invalid_passport_id
+    assert_equal(false, valid_passport_id?('pid:0123456789'))
+  end
+
+  def test_part_two
+    assert_equal(188, part_two(input))
   end
 end
 
