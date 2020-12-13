2170e95419e1e04c0dc3abd5c72f7e2514bc8ab1
Rider Sargent <rider.sargent@gmail.com>
2020-12-13 11:48:06 +0000

    Day 7, part 2, and small part 1 refactor.

--- day07/code.rb
+++ day07/code.rb
@@ -8,6 +8,14 @@ def part_one(input)
   graph.find_ancestors('shiny gold')
 end
 
+def part_two(input)
+  graph = Graph.new
+
+  input.each { |line| graph.parse(line) }
+
+  graph.find_descendants('shiny gold')
+end
+
 class Graph
   attr_accessor :vertices, :visited
 
@@ -20,26 +28,28 @@ class Graph
     parent_color, children = line.split(' bags contain ')
     children = children.split(', ')
 
-    add_bag(parent_color)
+    add_vertex(parent_color)
 
     children.each do |child|
-      child.match(/\d+ (\w+ \w+)/) do |m|
-        child_color = m[1]
-        add_bag(child_color)
-        add_child(parent_color, child_color)
+      child.match(/(\d+) (\w+ \w+)/) do |m|
+        num_bags = m[1]
+        child_color = m[2]
+        add_vertex(child_color)
+        add_edge(parent_color, child_color, num_bags)
       end
     end
   end
 
-  def add_bag(color)
+  def add_vertex(color)
     vertices << Vertex.new(color) if find_vertex_by_color(color).nil?
   end
 
-  def add_child(parent_color, child_color)
+  def add_edge(parent_color, child_color, weight)
     parent_index = vertices.index { |v| v.color == parent_color }
     child_index = vertices.index { |v| v.color == child_color }
 
     vertices[parent_index].children[child_index] = true
+    vertices[parent_index].weights[child_index] = weight
     vertices[child_index].parents[parent_index] = true
   end
 
@@ -66,15 +76,34 @@ class Graph
 
     ancestors
   end
+
+  def find_descendants(color)
+    current_vertex = find_vertex_by_color(color)
+    current_children = current_vertex.children
+
+    descendants = 0
+
+    current_children.each_with_index do |is_child, i|
+      next unless is_child
+
+      child = vertices[i]
+
+      descendants += current_vertex.weights[i].to_i
+      descendants += current_vertex.weights[i].to_i * find_descendants(child.color)
+    end
+
+    descendants
+  end
 end
 
 class Vertex
-  attr_accessor :color, :children, :parents
+  attr_reader :color, :children, :parents, :weights
 
   def initialize(color)
     @color = color
     @parents = []
     @children = []
+    @weights = []
   end
 end
 
--- day07/tests.rb
+++ day07/tests.rb
@@ -9,7 +9,7 @@ def input
   File.open(file_path).read.split("\n")
 end
 
-def example_input
+def example_input_one
   [
     'light red bags contain 1 bright white bag, 2 muted yellow bags.',
     'dark orange bags contain 3 bright white bags, 4 muted yellow bags.',
@@ -23,13 +23,37 @@ def example_input
   ]
 end
 
+def example_input_two
+  [
+    'shiny gold bags contain 2 dark red bags.',
+    'dark red bags contain 2 dark orange bags.',
+    'dark orange bags contain 2 dark yellow bags.',
+    'dark yellow bags contain 2 dark green bags.',
+    'dark green bags contain 2 dark blue bags.',
+    'dark blue bags contain 2 dark violet bags.',
+    'dark violet bags contain no other bags.'
+  ]
+end
+
 class TestDay < Minitest::Test
-  def test_part_one_example
-    assert_equal(4, part_one(example_input))
+  def test_example_one
+    assert_equal(4, part_one(example_input_one))
   end
 
   def test_part_one
     assert_equal(378, part_one(input))
+  end
+
+  def test_example_two
+    assert_equal(32, part_two(example_input_one))
+  end
+
+  def test_example_three
+    assert_equal(126, part_two(example_input_two))
+  end
+
+  def test_part_two
+    assert_equal(27_526, part_two(input))
   end
 end
 
