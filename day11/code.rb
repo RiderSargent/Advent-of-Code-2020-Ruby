088cd3c557574dfbeaa3dfd2613336f1b01179ec
Rider Sargent <rider.sargent@gmail.com>
2020-12-15 17:59:43 +0000

    Day 11, part 2.

--- day11/code.rb
+++ day11/code.rb
@@ -12,6 +12,18 @@ def part_one(input)
   count_seated(this_round)
 end
 
+def part_two(input)
+  last_round = [ '' ]
+  this_round = input
+
+  while last_round != this_round
+    last_round = this_round
+    this_round = next_round_part_two(last_round)
+  end
+
+  count_seated(this_round)
+end
+
 def count_seated(seats)
   seats.map { |row| row.count('#') }.sum
 end
@@ -60,6 +72,92 @@ def seated_around(r, c, seats)
   result += 1 if in_bounds?(r + 1, c + 1, seats) && seats[r + 1][c + 1] == '#'
 
   result
+end
+
+def next_round_part_two(seats)
+  # If a seat is empty (L) and there are no occupied seats adjacent to it,
+  #   the seat becomes occupied.
+  # If a seat is occupied (#) and five or more seats visible to it are also occupied,
+  #   the seat becomes empty.
+  # Otherwise, the seat's state does not change.
+
+  seats = seats.map(&:chars)
+  num_rows = seats.length
+  num_cols = seats.first.length
+
+  nexts = seats.clone.map(&:clone)
+
+  0.upto(num_rows - 1) do |r|
+    0.upto(num_cols - 1) do |c|
+      next if seats[r][c] == '.'
+
+      is_empty = seats[r][c] == 'L'
+      visibly_taken = visible_from(r, c, seats)
+
+      if is_empty && visibly_taken.zero?
+        nexts[r][c] = '#'
+      elsif !is_empty && visibly_taken > 4
+        nexts[r][c] = 'L'
+      end
+    end
+  end
+
+  nexts.map(&:join)
+end
+
+def visible_from(r, c, seats)
+  [
+    search('up', r, c, seats),
+    search('up_and_right', r, c, seats),
+    search('right', r, c, seats),
+    search('down_and_right', r, c, seats),
+    search('down', r, c, seats),
+    search('down_and_left', r, c, seats),
+    search('left', r, c, seats),
+    search('up_and_left', r, c, seats)
+  ].sum
+end
+
+def search(dir, r, c, seats)
+  loop do
+    case dir
+    when 'up'
+      r -= 1
+    when 'down'
+      r += 1
+    when 'left'
+      c -= 1
+    when 'right'
+      c += 1
+    when 'up_and_right'
+      r -= 1
+      c += 1
+    when 'down_and_right'
+      r += 1
+      c += 1
+    when 'down_and_left'
+      r += 1
+      c -= 1
+    when 'up_and_left'
+      r -= 1
+      c -= 1
+    end
+
+    return 0 if !in_bounds?(r, c, seats) || seats[r][c] == 'L'
+    return 1 if seats[r][c] == '#'
+  end
+  0
+end
+
+def search_up_and_left(r, c, seats)
+  while in_bounds?(r - 1, c - 1, seats)
+    r -= 1
+    c -= 1
+
+    return 1 if seats[r][c] == '#'
+    return 0 if seats[r][c] == 'L'
+  end
+  0
 end
 
 def in_bounds?(r, c, seats)
--- day11/tests.rb
+++ day11/tests.rb
@@ -24,7 +24,7 @@ def example_input
   ]
 end
 
-def expected_after_first_round
+def part_one_round_one_expected
   [
     '#.##.##.##',
     '#######.##',
@@ -39,7 +39,7 @@ def expected_after_first_round
   ]
 end
 
-def expected_after_second_round
+def part_one_round_two_expected
   [
     '#.LL.L#.##',
     '#LLLLLL.L#',
@@ -54,7 +54,7 @@ def expected_after_second_round
   ]
 end
 
-def expected_after_third_round
+def part_one_round_three_expected
   [
     '#.##.L#.##',
     '#L###LL.L#',
@@ -69,7 +69,7 @@ def expected_after_third_round
   ]
 end
 
-def expected_after_fourth_round
+def part_one_round_four_expected
   [
     '#.#L.L#.##',
     '#LLL#LL.L#',
@@ -84,7 +84,7 @@ def expected_after_fourth_round
   ]
 end
 
-def expected_after_fifth_round
+def part_one_round_five_expected
   [
     '#.#L.L#.##',
     '#LLL#LL.L#',
@@ -99,41 +99,122 @@ def expected_after_fifth_round
   ]
 end
 
+def part_two_round_two_expected
+  [
+    '#.LL.LL.L#',
+    '#LLLLLL.LL',
+    'L.L.L..L..',
+    'LLLL.LL.LL',
+    'L.LL.LL.LL',
+    'L.LLLLL.LL',
+    '..L.L.....',
+    'LLLLLLLLL#',
+    '#.LLLLLL.L',
+    '#.LLLLL.L#'
+  ]
+end
+
+def visible_from_example_input
+  [
+    '.......#.',
+    '...#.....',
+    '.#.......',
+    '.........',
+    '..#L....#',
+    '....#....',
+    '.........',
+    '#........',
+    '...#.....'
+  ]
+end
+
+def no_visible_seats_example_input
+  [
+    '.##.##.',
+    '#.#.#.#',
+    '##...##',
+    '...L...',
+    '##...##',
+    '#.#.#.#',
+    '.##.##.'
+  ]
+end
+
+def only_empty_seats_visible_example_input
+  [
+    '#..#..#',
+    '.L.L.L.',
+    '.......',
+    '#L.L.L#',
+    '.......',
+    '.L.L.L.',
+    '#..#..#'
+  ]
+end
+
 class TestDay < Minitest::Test
-  def test_next_round_for_round_one
-    assert_equal(expected_after_first_round, next_round(example_input))
+  def test_next_round_for_example_input
+    assert_equal(part_one_round_one_expected, next_round(example_input))
   end
 
-  def test_example_one_second_round
-    assert_equal(expected_after_second_round, next_round(expected_after_first_round))
+  def test_second_round_example_one
+    assert_equal(part_one_round_two_expected, next_round(part_one_round_one_expected))
   end
 
-  def test_example_one_third_round
-    assert_equal(expected_after_third_round, next_round(expected_after_second_round))
+  def test_third_round_example_one
+    assert_equal(part_one_round_three_expected, next_round(part_one_round_two_expected))
   end
 
-  def test_example_one_fourth_round
-    assert_equal(expected_after_fourth_round, next_round(expected_after_third_round))
+  def test_fourth_round_example_one
+    assert_equal(part_one_round_four_expected, next_round(part_one_round_three_expected))
   end
 
-  def test_example_one_fifth_round
-    assert_equal(expected_after_fifth_round, next_round(expected_after_fourth_round))
+  def test_fifth_round_example_one
+    assert_equal(part_one_round_five_expected, next_round(part_one_round_four_expected))
   end
 
-  def test_example_one_sixth_round
-    assert_equal(expected_after_fifth_round, next_round(expected_after_fifth_round))
+  def test_sixth_round_example_one
+    assert_equal(part_one_round_five_expected, next_round(part_one_round_five_expected))
   end
 
   def test_count_seated
-    assert_equal(37, count_seated(expected_after_fifth_round))
+    assert_equal(37, count_seated(part_one_round_five_expected))
   end
 
   def test_example
     assert_equal(37, part_one(example_input))
   end
 
+  def test_visible_from
+    assert_equal(8, visible_from(4, 3, visible_from_example_input))
+  end
+
+  def test_no_visible_seats_example
+    assert_equal(0, visible_from(3, 3, no_visible_seats_example_input))
+  end
+
+  def test_only_empty_seats_visible_example
+    assert_equal(0, visible_from(3, 3, only_empty_seats_visible_example_input))
+  end
+
   def test_part_one
     assert_equal(2_418, part_one(input))
+  end
+
+  def test_next_round_two_for_example_input
+    assert_equal(part_one_round_one_expected, next_round_part_two(example_input))
+  end
+
+  def test_second_round_for_example_two
+    assert_equal(part_two_round_two_expected, next_round_part_two(part_one_round_one_expected))
+  end
+
+  def test_part_two_example
+    assert_equal(26, part_two(example_input))
+  end
+
+  def test_part_two
+    assert_equal(2_144, part_two(input))
   end
 end
 
