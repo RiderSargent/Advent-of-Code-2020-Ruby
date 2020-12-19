ec60a277111941b79c8246e1d6d44474bb53cfa1
Rider Sargent <rider.sargent@gmail.com>
2020-12-19 19:22:05 +0000

    Day 14, part 2 and part 1 refactor.

--- day14/code.rb
+++ day14/code.rb
@@ -2,7 +2,7 @@
 
 def part_one(input)
   mask = ''
-  mem = []
+  mem = {}
 
   input.map do |line|
     line.match(/(mask|mem\[(\d+)\]) = (\S+)/) do |m|
@@ -17,20 +17,59 @@ def part_one(input)
     end
   end
 
-  sum(mem)
+  mem.values.sum
 end
 
-def sum(mem)
-  mem.filter { |m| !m.nil? }.reduce(0) do |acc, address|
-    acc + address.to_i(2)
+def part_two(input)
+  mask = ''
+  mem = {}
+
+  input.each do |line|
+    line.match(/(mask|mem\[(\d+)\]) = (\S+)/) do |m|
+      address = m[2]
+      value = m[3]
+
+      if address.nil?
+        mask = value
+      else
+        decode_addresses(address.to_i, mask).each do |a|
+          mem[a] = value.to_i
+        end
+      end
+    end
   end
+
+  mem.values.sum
+end
+
+def decode_addresses(address, mask)
+  address = to_bin(address)
+  x_count = mask.count('X')
+
+  masked_address = apply_mask_to_address(mask, address)
+
+  x_combinations = (2**x_count - 1).downto(0).map do |val|
+    to_bin(val, x_count)
+  end
+
+  x_combinations.map do |combo|
+    masked_address.gsub('X', '%s') % combo.split('')
+  end
+end
+
+def apply_mask_to_address(mask, address)
+  mask.chars.zip(address.chars).map { |m, a| m == '0' ? a : m }.join
 end
 
 def apply_mask(mask, value)
-  b_value = value.to_i.to_s(2).rjust(36, '0')
+  b_value = to_bin(value)
   (0...36).each do |i|
     b_value[i] = mask[i] unless mask[i] == 'X'
   end
-  b_value
+  b_value.to_i(2)
+end
+
+def to_bin(n, length = 36)
+  n.to_i.to_s(2).rjust(length, '0')
 end
 
--- day14/tests.rb
+++ day14/tests.rb
@@ -9,7 +9,7 @@ def input
   File.open(file_path).read.split("\n")
 end
 
-def example_input
+def example_one_input
   [
     'mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
     'mem[8] = 11',
@@ -18,13 +18,30 @@ def example_input
   ]
 end
 
+def example_two_input
+  [
+    'mask = 000000000000000000000000000000X1001X',
+    'mem[42] = 100',
+    'mask = 00000000000000000000000000000000X0XX',
+    'mem[26] = 1'
+  ]
+end
+
 class TestDay < Minitest::Test
   def test_example_one
-    assert_equal(165, part_one(example_input))
+    assert_equal(165, part_one(example_one_input))
   end
 
   def test_part_one
     assert_equal(9_879_607_673_316, part_one(input))
+  end
+
+  def test_example_two
+    assert_equal(208, part_two(example_two_input))
+  end
+
+  def test_part_two
+    assert_equal(3_435_342_392_262, part_two(input))
   end
 end
 
