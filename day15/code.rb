ab26db1541a7fbe56ca918ac7f627729b1ece748
Rider Sargent <rider.sargent@gmail.com>
2020-12-21 17:51:57 +0000

    Day 15.

---
+++ day15/code.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+def part_one(input)
+  nth_number(input, 2020)
+end
+
+def part_two(input)
+  nth_number(input, 30_000_000)
+end
+
+def nth_number(input, target)
+  prev_positions = {}
+  prev_number = nil
+
+  input.each_with_index do |number, i|
+    prev_positions[prev_number] = i unless prev_number.nil?
+    prev_number = number
+  end
+
+  (input.size..target).each do |i|
+    next_number = nil
+    prev_position = prev_positions[prev_number]
+
+    next_number = prev_position.nil? ? 0 : i - prev_position
+    prev_positions[prev_number] = i
+
+    return prev_number if i == target
+    prev_number = next_number
+  end
+end
+
---
+++ day15/tests.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'code'
+
+def input
+  [ 1, 20, 8, 12, 0, 14 ]
+end
+
+def example_one_input
+  [ 0, 3, 6 ]
+end
+
+class TestDay < Minitest::Test
+  def test_example_one
+    assert_equal(436, part_one(example_one_input))
+  end
+
+  def test_part_one
+    assert_equal(492, part_one(input))
+  end
+
+  def test_example_two
+    assert_equal(175_594, part_two(example_one_input))
+  end
+
+  def test_part_two
+    assert_equal(63_644, part_two(input))
+  end
+end
+
