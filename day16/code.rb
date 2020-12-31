ee4f371e25113d1be501dd75460423751a2aaa38
Rider Sargent <rider.sargent@gmail.com>
2020-12-31 12:28:05 +0000

    Refactor Day 16.

--- day16/code.rb
+++ day16/code.rb
@@ -1,40 +1,6 @@
 # frozen_string_literal: true
 
-def part_one(input)
-  input_parts = input.slice_when { |before, _after| before.empty? }.to_a
-
-  rules =
-    input_parts
-      .first
-      .filter { |line| !line.empty? }
-      .map { |line| line.split(': ') }
-      .map do |name, unparsed_rule|
-        [
-          name,
-          unparsed_rule
-            .split(' or ')
-            .map { |range| range.split('-').map(&:to_i) }
-            .map { |min, max| (min..max) }
-        ]
-      end
-
-  nearby_tickets =
-    input_parts
-      .last
-      .tap(&:shift)
-      .map { |ticket| ticket.split(',').map(&:to_i) }
-
-  nearby_tickets.sum do |ticket_numbers|
-    invalid_numbers = ticket_numbers.filter do |number|
-      rules.none? do |name, ranges|
-        ranges.any? { |range| range.include? number }
-      end
-    end
-    invalid_numbers.sum
-  end
-end
-
-def part_two(input)
+def parse(input)
   input_parts = input.slice_when { |before, _after| before.empty? }.to_a
 
   rules =
@@ -63,6 +29,24 @@ def part_two(input)
       .tap(&:shift)
       .map { |ticket| ticket.split(',').map(&:to_i) }
 
+  [ rules, my_ticket, nearby_tickets ]
+end
+
+def part_one(input)
+  rules, _my_ticket, nearby_tickets = parse(input)
+
+  nearby_tickets.sum do |ticket_numbers|
+    invalid_numbers = ticket_numbers.filter do |number|
+      rules.none? do |name, ranges|
+        ranges.any? { |range| range.include? number }
+      end
+    end
+    invalid_numbers.sum
+  end
+end
+
+def part_two(input)
+  rules, my_ticket, nearby_tickets = parse(input)
 
   valid_nearby_tickets =
     nearby_tickets.filter do |ticket_numbers|
