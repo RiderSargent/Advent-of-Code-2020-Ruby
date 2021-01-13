ead06f552db896dc755211825a3551a16416a0cc
Rider Sargent <rider.sargent@gmail.com>
2021-01-13 19:52:05 +0000

    Day 17, part 2.

--- day17/code.rb
+++ day17/code.rb
@@ -19,6 +19,25 @@ def part_one(input)
   active_cubes.length
 end
 
+def part_two(input)
+  initial_plane = input.map { |l| l.split('') }
+
+  active_cubes = {}
+  initial_plane.each_with_index do |row, r|
+    row.each_with_index do |char, c|
+      next if char != '#'
+
+      active_cubes[[ r, c, 10, 10 ]] = true
+    end
+  end
+
+  6.times do
+    active_cubes = four_d_tick(active_cubes)
+  end
+
+  active_cubes.length
+end
+
 def get_neighbor_coords(current_coords)
   x, y, z = current_coords
 
@@ -51,6 +70,42 @@ def get_neighbor_coords(current_coords)
     [  0,  0,  1 ]
   ].map do |dx, dy, dz|
     [ x + dx, y + dy, z + dz ]
+  end
+end
+
+def get_four_d_neighbor_coords(current_coords)
+  x, y, z, w = current_coords
+
+  [
+    [ -1, -1, -1, -1 ], [ -1, -1, -1, 1 ], [ -1, -1, -1, 0 ],
+    [ -1, -1,  1, -1 ], [ -1, -1,  1, 1 ], [ -1, -1,  1, 0 ],
+    [ -1, -1,  0, -1 ], [ -1, -1,  0, 1 ], [ -1, -1,  0, 0 ],
+    [ -1,  1, -1, -1 ], [ -1,  1, -1, 1 ], [ -1,  1, -1, 0 ],
+    [ -1,  1,  1, -1 ], [ -1,  1,  1, 1 ], [ -1,  1,  1, 0 ],
+    [ -1,  1,  0, -1 ], [ -1,  1,  0, 1 ], [ -1,  1,  0, 0 ],
+    [ -1,  0, -1, -1 ], [ -1,  0, -1, 1 ], [ -1,  0, -1, 0 ],
+    [ -1,  0,  1, -1 ], [ -1,  0,  1, 1 ], [ -1,  0,  1, 0 ],
+    [ -1,  0,  0, -1 ], [ -1,  0,  0, 1 ], [ -1,  0,  0, 0 ],
+    [  1, -1, -1, -1 ], [  1, -1, -1, 1 ], [  1, -1, -1, 0 ],
+    [  1, -1,  1, -1 ], [  1, -1,  1, 1 ], [  1, -1,  1, 0 ],
+    [  1, -1,  0, -1 ], [  1, -1,  0, 1 ], [  1, -1,  0, 0 ],
+    [  1,  1, -1, -1 ], [  1,  1, -1, 1 ], [  1,  1, -1, 0 ],
+    [  1,  1,  1, -1 ], [  1,  1,  1, 1 ], [  1,  1,  1, 0 ],
+    [  1,  1,  0, -1 ], [  1,  1,  0, 1 ], [  1,  1,  0, 0 ],
+    [  1,  0, -1, -1 ], [  1,  0, -1, 1 ], [  1,  0, -1, 0 ],
+    [  1,  0,  1, -1 ], [  1,  0,  1, 1 ], [  1,  0,  1, 0 ],
+    [  1,  0,  0, -1 ], [  1,  0,  0, 1 ], [  1,  0,  0, 0 ],
+    [  0, -1, -1, -1 ], [  0, -1, -1, 1 ], [  0, -1, -1, 0 ],
+    [  0, -1,  1, -1 ], [  0, -1,  1, 1 ], [  0, -1,  1, 0 ],
+    [  0, -1,  0, -1 ], [  0, -1,  0, 1 ], [  0, -1,  0, 0 ],
+    [  0,  1, -1, -1 ], [  0,  1, -1, 1 ], [  0,  1, -1, 0 ],
+    [  0,  1,  1, -1 ], [  0,  1,  1, 1 ], [  0,  1,  1, 0 ],
+    [  0,  1,  0, -1 ], [  0,  1,  0, 1 ], [  0,  1,  0, 0 ],
+    [  0,  0, -1, -1 ], [  0,  0, -1, 1 ], [  0,  0, -1, 0 ],
+    [  0,  0,  1, -1 ], [  0,  0,  1, 1 ], [  0,  0,  1, 0 ],
+    [  0,  0,  0, -1 ], [  0,  0,  0, 1 ]
+  ].map do |dx, dy, dz, dw|
+    [ x + dx, y + dy, z + dz, w + dw ]
   end
 end
 
@@ -89,3 +144,31 @@ def tick(active_cubes)
   next_active_cubes
 end
 
+def four_d_tick(active_cubes)
+  next_active_cubes = {}
+  inactives_to_check = {}
+
+  # If a cube is active and exactly 2 or 3 of its neighbors are also active,
+  # the cube remains active. Otherwise, the cube becomes inactive.
+  active_cubes.each_key do |current_coords|
+    neighbors_coords = get_four_d_neighbor_coords(current_coords)
+    if [ 2, 3 ].include?(count_active_neighbors(active_cubes, neighbors_coords))
+      next_active_cubes[current_coords] = true
+    end
+
+    neighbors_coords.each do |neighbor_coords|
+      inactives_to_check[neighbor_coords] = true
+    end
+  end
+
+  # If a cube is inactive but exactly 3 of its neighbors are active, the cube
+  # becomes active. Otherwise, the cube remains inactive.
+  inactives_to_check.each_key do |current_coords|
+    neighbors_coords = get_four_d_neighbor_coords(current_coords)
+    if count_active_neighbors(active_cubes, neighbors_coords) == 3
+      next_active_cubes[current_coords] = true
+    end
+  end
+
+  next_active_cubes
+end
--- day17/tests.rb
+++ day17/tests.rb
@@ -33,5 +33,13 @@ class TestDay < Minitest::Test
   def test_part_one
     assert_equal(401, part_one(input))
   end
+
+  def test_example_two
+    assert_equal(848, part_two(example_input))
+  end
+
+  def test_part_two
+    assert_equal(2_224, part_two(input))
+  end
 end
 
